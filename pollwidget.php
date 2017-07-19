329f77d55e0d3f5b5e37a6adb2ac18e10f195e2e
casanogab <casanogab@hotmail.com>
2017-07-19 10:08:39 -0400

    fonction de rendu des données effectuées

--- pollwidget.php
+++ pollwidget.php
@@ -11,7 +11,9 @@ class Poll_Widget extends WP_Widget
     public function __construct()
     {
       parent::__construct('gab_Vote_Fruits', 'Vote_Fruits', array('description' => 'Un formulaire de vote sur les fruits favoris.'));
-      add_action('monHook', array($this, 'updateDansPollResults'));  
+      add_action('monHookUpdateDansPollResults', array($this, 'updateDansPollResults')); 
+      add_action('monHookQuestionnaireDePollResults', array($this, 'questionnaireDePollResults'));
+      add_action('monHookRenduDePollResults', array($this, 'renduDePollResults')); 
     }
 
 
@@ -25,28 +27,13 @@ class Poll_Widget extends WP_Widget
       echo $args['before_title'];
       echo apply_filters('widget_title', $instance['title']);
       echo $args['after_title'];
-      ?>
-      <form action="" method="post">
-          <p>
-
-                <input type="radio" name="rdb_fruits" value="1"> oranges<br>
-                <input type="radio" name="rdb_fruits" value="2"> pommes <br>
-                <input type="radio" name="rdb_fruits" value="3"> poires <br>
-                <input type="radio" name="rdb_fruits" value="4"> pèches <br>
-
-          </p>
-          <input type="submit" value="envoyer"/>
-      </form>
-      <?php
+      do_action('monHookQuestionnaireDePollResults');
       
         if(isset($_POST['rdb_fruits'])){
-          #if (1==1){
            $vote = $_POST['rdb_fruits']; 
-             # echo "fruit choisi".$voteEffectue;
-             do_action('monHook', $vote);
-
-
+             do_action('monHookUpdateDansPollResults', $vote);
         }
+       do_action('monHookRenduDePollResults');
       echo $args['after_widget'];
     }
 
@@ -57,11 +44,52 @@ class Poll_Widget extends WP_Widget
             $totalTmp = $row->total + 1;
             $wpdb->update("{$wpdb->prefix}poll_results", array('option_id' => $vote, 'total' => $totalTmp ),array('option_id' => $vote));         
     }
+ 
+  public function questionnaireDePollResults()
+    {
+    ?>
+      <form action="" method="post">
+          <p>
+                <input type="radio" name="rdb_fruits" value="1"> oranges<br>
+                <input type="radio" name="rdb_fruits" value="2"> pommes <br>
+                <input type="radio" name="rdb_fruits" value="3"> poires <br>
+                <input type="radio" name="rdb_fruits" value="4"> pêches <br>
 
+          </p>
+          <input type="submit" value="envoyer"/>
+      </form>
+      <?php        
+    }
+
+  public function renduDePollResults()
+    {
+            global $wpdb;
+            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = 1");
+    ?>
+        <label>Résultats: </label>
+          <p>
+              <label>Oranges: </label><?php echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = 1")->total;?> votes <br>
+              <label>Pommes:  </label><?php echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = 2")->total;?> votes <br>
+              <label>Poires:  </label><?php echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = 3")->total;?> votes <br>
+              <label>Pêches:  </label><?php echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = 4")->total;?> votes <br>
+          </p>
+
+      <?php        
+    }
 
     /**
-     * Affichage du formulaire dans l'administration PAS POUR LE MOMENT
+     * Affichage du formulaire dans l'administration 
      */
     public function form($instance)
-    {}
+    {
+      #permet de setter le titre dans apparence de widget DICI
+      $title = isset($instance['title']) ? $instance['title'] : '';
+      ?>
+      <p>
+          <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+          <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo  $title; ?>" />
+      </p>
+      <?php
+      #Jusqu'à ici Sondage
+    }
 }
