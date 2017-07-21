3fea287a3ee0848da6a42a43b0c62ce46662066a
casanogab <casanogab@hotmail.com>
2017-07-21 12:09:08 -0400

    le code fonctionne

--- pollwidget.php
+++ pollwidget.php
@@ -5,128 +5,153 @@
  */
 class Poll_Widget extends WP_Widget
 {
-    /**
-     * Constructeur
-     */
-    public function __construct()
+  /**
+   * Constructeur
+   */
+  public function __construct()
+  {
+    $this->setLeCookie();
+    parent::__construct('gab_Vote_Fruits', 'Vote_Fruits', array('description' => 'Un formulaire de vote sur les fruits favoris.'));
+    add_action('monHookUpdateDansPollResults', array($this, 'updateDansPollResults')); 
+    add_action('monHookQuestionnaireDePollResults', array($this, 'questionnaireDePollResults'));
+    add_action('monHookRenduDePollResults', array($this, 'renduDePollResults')); 
+
+  }
+
+  public function setLeCookie()
+  {
+    if(isset($_POST['rdb_fruits'])){
+       $vis_username = "allo";
+       $vis_value = "ceci";
+       setcookie( $vis_username, $vis_value, time() + ( 7 * DAY_IN_SECONDS ), COOKIEPATH, COOKIE_DOMAIN);          
+       header('Location: '.$_SERVER['PHP_SELF']);  
+        die;
+      }
+  }
+  /**
+   * Affichage du widget Du coté client
+   */
+
+  public function widget($args, $instance)
+  {
+    echo $args['before_widget'];
+
+    echo $args['before_title'];
+    echo apply_filters('widget_title', $instance['title']);
+    echo $args['after_title'];
+
+   
+
+   
+    do_action('monHookQuestionnaireDePollResults');
+    if(isset($_POST['rdb_fruits']))
     {
-
-      parent::__construct('gab_Vote_Fruits', 'Vote_Fruits', array('description' => 'Un formulaire de vote sur les fruits favoris.'));
-      add_action('monHooksetLeCookie', array($this, 'setLeCookie'));
-      #   $v_username = "allo";
-       #  $v_value = "ceci";
-       #  setcookie( $v_username, $v_value, 30 * DAYS_IN_SECONDS); 
-      #$this->setLeCookie();   
-      add_action('monHookUpdateDansPollResults', array($this, 'updateDansPollResults')); 
-      add_action('monHookQuestionnaireDePollResults', array($this, 'questionnaireDePollResults'));
-      add_action('monHookRenduDePollResults', array($this, 'renduDePollResults')); 
-
-      
+      $vote = $_POST['rdb_fruits']; 
+      do_action('monHookUpdateDansPollResults', $vote);
     }
+    do_action('monHookRenduDePollResults');
 
-    public function setLeCookie()
-    {
-      if(isset($_POST['rdb_fruits'])){
-         $v_username = "allo";
-         $v_value = "ceci";
-         setcookie( $v_username, $v_value, 30 * DAYS_IN_SECONDS);    
-        }
-    }
-    /**
-     * Affichage du widget Du coté client
-     */
+    echo $args['after_widget'];
+  }
 
-    public function widget($args, $instance)
-    {
-  
-      echo $args['before_widget'];
-      echo $args['before_title'];
-      echo apply_filters('widget_title', $instance['title']);
-      echo $args['after_title'];
-      do_action('monHookQuestionnaireDePollResults');
-      
-        if(isset($_POST['rdb_fruits'])){
-           $vote = $_POST['rdb_fruits']; 
-             do_action('monHookUpdateDansPollResults', $vote);
-         
-         }
-       do_action('monHookRenduDePollResults');
-      echo $args['after_widget'];
-    }
+  public function updateDansPollResults($vote)
+  {
+    global $wpdb;
+    $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = '$vote'");
+    $totalTmp = $row->total + 1;
+    $wpdb->update("{$wpdb->prefix}poll_results", array('option_id' => $vote, 'total' => $totalTmp ),array('option_id' => $vote));     
+  }
 
-    public function updateDansPollResults($vote)
-    {
-            global $wpdb;
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = '$vote'");
-            $totalTmp = $row->total + 1;
-            $wpdb->update("{$wpdb->prefix}poll_results", array('option_id' => $vote, 'total' => $totalTmp ),array('option_id' => $vote));
-            
-                    
-                   
-    }
- 
   public function questionnaireDePollResults()
-    {
-    ?>
-      <form action="" method="post">
-          <p>
-             <?php
-               global $wpdb;
-                $totalOptions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}poll_options" );   
-                for ($cpt = 1; $cpt <= $totalOptions ; $cpt++) {
-                    $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_options WHERE id = '$cpt'"); 
-                    echo "<input type=\"radio\" name=\"rdb_fruits\" value=\"$cpt\">$row->label<br><br>";
-                }
-              ?>
-          </p>
-          <input type="submit" value="envoyer"/>
-      </form>
-      <?php        
+  {
+  ?>
+    <form action="" method="post">
+        <p>
+           <?php
+             global $wpdb;
+              $totalOptions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}poll_options" );   
+              for ($cpt = 1; $cpt <= $totalOptions ; $cpt++) {
+                  $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_options WHERE id = '$cpt'"); 
+                  echo "<input type=\"radio\" name=\"rdb_fruits\" value=\"$cpt\">$row->label<br><br>";
+              }
+            ?>
+        </p>
+    <?php 
+    $vis_username = "allo";
+    $vis_value = "ceci";
+    if(!isset($_COOKIE[$vis_username])) {
+           echo"<input type='submit' value='enregistrer mon vote'>";
     }
+     ?>
+    </form>
+    <?php        
+  }
 
   public function renduDePollResults()
-    {
-
-      $v_username = "allo";
-      $v_value = "ceci";
-       if(!isset($_COOKIE[$v_username])) {
-        echo "The cookie: '" . $v_username . "' is not set.";
-      } else {
-        echo "The cookie '" . $v_username . "' is set.";
-        echo "Cookie is:  " . $_COOKIE[$v_username];
-      }
+  {
 
 
-      ?>
-        <br><label>Résultats: </label><br><br>
-          <p>
-            <?php
-             global $wpdb;
-             $totalOptions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}poll_options" );  
-             for ($cpt = 1; $cpt <= $totalOptions ; $cpt++) {
-                  echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_options WHERE id = '$cpt'")->label." ";
-                  echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = '$cpt'")->total." votes<br><br>";
-              } 
-            ?>
-          </p>
 
-      <?php        
-    }
+    ?>
+      <br><label>Résultats: </label><br><br>
+        <p>
+          <?php
+           global $wpdb;
+           $totalOptions = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}poll_options" );  
+           for ($cpt = 1; $cpt <= $totalOptions ; $cpt++) {
+                echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_options WHERE id = '$cpt'")->label." ";
+                echo $wpdb->get_row("SELECT * FROM {$wpdb->prefix}poll_results WHERE option_id = '$cpt'")->total." votes<br><br>";
+            } 
+          ?>
+        </p>
 
-    /**
-     * Affichage du formulaire dans l'administration 
-     * oÙ C'EST CALLER CA %???
-     */
-    public function form($instance)
-    {
-      #permet de setter le titre dans apparence de widget DICI
-      $title = isset($instance['title']) ? $instance['title'] : '';
-      ?>
-      <p>
-          <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-          <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo  $title; ?>" />
-      </p>
-      <?php
-      #Jusqu'à ici Sondage
-    }  
+    <?php        
+  }
+
+  /**
+   * Affichage du formulaire dans l'administration 
+   * oÙ C'EST CALLER CA %???
+   */
+  public function form($instance)
+  {
+    #permet de setter le titre dans apparence de widget DICI
+    $title = isset($instance['title']) ? $instance['title'] : '';
+    ?>
+    <p>
+        <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo  $title; ?>" />
+    </p>
+    <?php
+    #Jusqu'à ici Sondage
+  }  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+vieu code
+
+       $vis_username = "allo";
+       $vis_value = "ceci";
+    if(!isset($_COOKIE[$vis_username])) {
+  echo "The cookie: '" . $vis_username . "' is not seti.";
+} else {
+  echo "The cookie '" . $vis_username . "' is set.";
+  echo "Cookie is:  " . $_COOKIE[$vis_username];
+  
+}
+*/
